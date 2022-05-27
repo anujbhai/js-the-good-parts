a0b75985addd60e8f2b5fc0e96db51621117d037
Anuj.Upadhyay <anuj.upadhyay@rsystems.com>
2022-05-27 23:40:16 +0530

    first commit

---
+++ objects/script01.js
@@ -0,0 +1,14 @@
+(function () {
+"use strict";
+
+console.log("Everything's working fine!");
+
+/* Object Literals */
+var empty_object = {};
+
+var stooge = {
+	first-name: "Jerome",
+	last-name: "Howard"
+};
+
+}) ();
