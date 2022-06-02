b136322ef61f9d43a6b2b82ff6716b4252b75fec
Anuj.Upadhyay <anuj.upadhyay@rsystems.com>
2022-06-02 12:46:46 +0530

    JSTGP-02-ch-04 - functions | recursion, walk the dom.

--- functions/script02.js
+++ functions/script02.js
@@ -72,4 +72,29 @@ var hanoi = function (disc, src, aux, des) {
 };
 hanoi(3, "Source", "Auxilary", "Destination");
 
+// walk the DOM
+var walk_the_DOM = function walk(node, func) {
+	func(node);
+	node = node.firstChild;
+	while (node) {
+		walk(node, func);
+		node = node.nextSibling;
+	}
+};
+
+var getElementsByAttribute = function (att, value) {
+	var results = [];
+	
+	walk_the_DOM(document.body, function (node) {
+		var actual = node.nodeType === 1 && node.getAttribute(att);
+		if (typeof actual === "string" && (actual === value || typeof value !== "string")) {
+			results.push(node);
+		}
+	});
+
+	return results;
+};
+
+console.log(getElementsByAttribute("data-spare", "just-coding"));
+
 }) ();
