9261cbeee5051ef1b43a6cc2a65fe85e79facebf
Anuj.Upadhyay <anuj.upadhyay@rsystems.com>
2022-06-17 20:42:29 +0530

    JSTGP-06-ch-08 - methods | array sort and function bind.

--- methods/script01_array_methods.js
+++ methods/script01_array_methods.js
@@ -136,4 +136,58 @@ n.sort(function (a, b) {
 });
 console.log(n);
 
+var m = ["aa", "bb", "a", 4, 8, 15, 16, 23, 42];
+m.sort(function (a, b) {
+	if (a === b) {
+		return 0;
+	}
+
+	if (typeof a === typeof b) {
+		return a < b ? -1 : 1;
+	}
+
+	return typeof a < typeof b ? -1 : 1;
+});
+console.log(m);
+
+// Function 'by' takes a member name string and returns
+// a comparision function that can be used to sort an
+// array of objects that contain that member.
+var by = function (name, minor) {
+	return function (o, p) {
+		var a, b;
+
+		if (typeof o === "object" && typeof p === "object" && o && p) {
+			a = o[name];
+			b = p[name];
+
+			if (a === b) {
+				return typeof minor === "function" ? minor(o, p) : 0;
+			}
+
+			if (typeof a === typeof b) {
+				return a < b ? -1 : 1;
+			}
+
+			return typeof a < typeof b ? -1 : 1;
+		} else {
+			throw {
+				name: "Error",
+				message: "Expected an object when sorting by " + name
+			};
+		}
+	};
+};
+
+var s = [
+	{first: "Joe", last: "Besser"},
+	{first: "Moe", last: "Howard"},
+	{first: "Joe", last: "DeRita"},
+	{first: "Shemp", last: "Howard"},
+	{first: "Larry", last: "Fine"},
+	{first: "Curly", last: "Howard"},
+];
+
+console.log(s.sort(by("last", by("first"))));
+
 }) ();
